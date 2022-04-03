7520478673c93445ccb3c6066a623afd63bc837c
91897 <saniasmk786@gmail.com>
2022-04-03 08:31:04 +0530

    first commit

---
+++ BasicProgram/src/com/tns/library/day19/Progrm.java
@@ -0,0 +1,27 @@
+package com.tns.library.day19;
+
+class Arithmetic {
+
+	  int add(int a, int b) {
+	    return a + b;
+	  }
+	}
+
+	class Adder extends Arithmetic {
+		
+	}
+
+	public class Progrm {
+
+	  public static void main(String[] args) {
+		  
+	    // Create a new Adder object
+	    Adder a = new Adder();
+
+	    // Print the name of the superclass 
+	    System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());
+
+	    // Print the result of 3 calls to Adder's `add(int,int)` method as 3 space-separated integers
+	    System.out.print(a.add(10, 32) + " " + a.add(10, 3) + " " + a.add(10, 10) + "\n");
+	  }
+	}
